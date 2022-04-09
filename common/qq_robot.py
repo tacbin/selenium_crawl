09dc5b586e2174410f7b00c13d044c8793f69b18
tacbin <2571761894@qq.com>
2022-04-09 11:17:43 +0800

    revert

--- common/qq_robot.py
+++ common/qq_robot.py
@@ -9,15 +9,14 @@ from common.common_instantce import CommonInstance
 class QQRobot:
     @staticmethod
     def send_group_msg(group: int, msg: List):
-        pass
-        # try:
-        #     while CommonInstance.App_IS_LOCKED:
-        #         time.sleep(random.randint(5, 15))
-        #     CommonInstance.App_IS_LOCKED = True
-        #     time.sleep(random.randint(1, 3))
-        #     CommonInstance.QQ_ROBOT.send_group_msg(group=group,
-        #                                            msg=msg)
-        # except Exception as e:
-        #     print('send_group_msg', e)
-        # finally:
-        #     CommonInstance.App_IS_LOCKED = False
+        try:
+            while CommonInstance.App_IS_LOCKED:
+                time.sleep(random.randint(5, 15))
+            CommonInstance.App_IS_LOCKED = True
+            time.sleep(random.randint(1, 3))
+            CommonInstance.QQ_ROBOT.send_group_msg(group=group,
+                                                   msg=msg)
+        except Exception as e:
+            print('send_group_msg', e)
+        finally:
+            CommonInstance.App_IS_LOCKED = False
