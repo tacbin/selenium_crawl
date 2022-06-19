98947cb1430e0f018642733d52706da22da69b02
tacbin <2571761894@qq.com>
2022-06-19 23:12:11 +0800

    zego

---
+++ crawl/monitor/agora.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import time
+from typing import List
+import re
+
+import miraicle
+from lxml import html
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+from common.common_instantce import CommonInstance
+from common.qq_robot import QQRobot
+from common_crawl import CommonCrawl
+
+
+class AgoraCrawl(CommonCrawl):
+    def __init__(self):
+        super().__init__()
+        self.result_map = {}
+        self.next_urls = []
+
+    def before_crawl(self, args, browser: WebDriver) -> WebDriver:
+        self.result_map = {}
+        self.next_urls = []
+        self.urls = ["https://www.agora.io/en/blog/"]
+        for url in self.urls:
+            self.result_map[url] = []
+        self.file_location = 'Agora'
+        self.is_save_img = False
+        self.mode = 1
+        return browser
+
+    def parse(self, browser: WebDriver):
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), 'wei ke parse start crawl..',
+              browser.current_url)
+        page = browser.page_source
+        etree = html.etree
+        selector = etree.HTML(page)
+        cards = selector.xpath(
+            "//a[@class='cursor-pointer rounded-lg hover:shadow-xl transition duration-200 max-w-sm bg-gray-500']")
+        for card in cards:
+            sel = etree.HTML(etree.tostring(card, method='html'))
+            url = sel.xpath(
+                "//a[@class='cursor-pointer rounded-lg hover:shadow-xl transition duration-200 max-w-sm bg-gray-500']/@href")
+            url = url[0] if len(url) > 0 else ''
+            url = url.replace('\n', '')
+            url = "https://www.100Agora.live" + url
+
+            date = sel.xpath("//p[@class='pl-2 text-gray-200 text-sm mb-2']/text()")
+            date = date[0] if len(date) > 0 else ''
+            date = date.replace('\n', '')
+
+            title = sel.xpath("//h1[@class='font-medium text-2xl px-2']/text()")
+            title = title[0] if len(title) > 0 else ''
+            title = title.replace('\n', '')
+            if CommonInstance.Redis_client.get(url) is not None:
+                continue
+
+            self.result_map[browser.current_url].append(AgoraResult(title, url, date))
+
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), 'wei ke parse end crawl..', browser.current_url)
+
+    def custom_send(self):
+        for url in self.result_map:
+            print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), 'wei ke start custom_send..', url)
+            for data in self.result_map[url]:
+                txt = '【100Agora.live】发文章啦\n' \
+                      '标题:%s\n' \
+                      '时间:%s\n' \
+                      '链接:%s' % (data.title, data.date, data.url)
+                QQRobot.send_group_Agorag(461936572, [miraicle.Plain(txt)])
+                QQRobot.send_group_Agorag(963961013, [miraicle.Plain(txt)])
+                CommonInstance.Redis_client.set(data.url, '')
+
+    def get_next_urls(self, browser: WebDriver) -> List[str]:
+        return self.next_urls
+
+
+class AgoraResult:
+    def __init__(self, title, url, date):
+        self.title = title
+        self.url = url
+        self.date = date
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from crawl.beike.beike_crawl import BeiKeCrawl
 from crawl.beike.cs_beike_crawl import CsBeiKeCrawl
 from crawl.code_task.wei_ke_crawl import WeiKeCrawl
 from crawl.code_task.zhu_ba_jie_crawl import ZhuBaJieCrawl
-from crawl.hok_task.dai_lian_ma_ma_crawl import DaiLianMaMaCrawl
+from crawl.monitor.ms import MsCrawl
 from middleware.init_middleware import init_middleware
 from threads.mq_thread import MqThreadControl
 from threads.qq_robot_thread import QQRobotThreadControl
@@ -29,15 +29,16 @@ if __name__ == '__main__':
     scheduler = BackgroundScheduler()
     scheduler.add_job(empty_run, 'interval', seconds=300)
     # 添加调度任务
-    zbj_crawl = ZhuBaJieCrawl()
-    scheduler.add_job(zbj_crawl.run, 'interval', seconds=300)
-    wei_ke_crawl = WeiKeCrawl()
-    scheduler.add_job(wei_ke_crawl.run, 'interval', seconds=350)
-    bei_ke_crawl = BeiKeCrawl()
-    scheduler.add_job(bei_ke_crawl.run, 'interval', seconds=400)
-    cs_bei_ke_crawl = CsBeiKeCrawl()
-    scheduler.add_job(cs_bei_ke_crawl.run, 'interval', seconds=450)
-
+    # zbj_crawl = ZhuBaJieCrawl()
+    # scheduler.add_job(zbj_crawl.run, 'interval', seconds=300)
+    # wei_ke_crawl = WeiKeCrawl()
+    # scheduler.add_job(wei_ke_crawl.run, 'interval', seconds=350)
+    # bei_ke_crawl = BeiKeCrawl()
+    # scheduler.add_job(bei_ke_crawl.run, 'interval', seconds=400)
+    # cs_bei_ke_crawl = CsBeiKeCrawl()
+    # scheduler.add_job(cs_bei_ke_crawl.run, 'interval', seconds=450)
+    ms = MsCrawl()
+    scheduler.add_job(ms.run, 'interval', seconds=100)
     # 启动调度任务
     print('启动调度任务')
     scheduler.start()
