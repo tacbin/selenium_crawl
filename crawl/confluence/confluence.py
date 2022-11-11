00fe1a85cbcde775cd95783e90c32a14e6240785
Weibin Liu <weibin.liu@shopee.com>
2022-11-11 18:21:56 +0800

    1

--- crawl/confluence/confluence.py
+++ crawl/confluence/confluence.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 import time
 from lxml import html
 from selenium.webdriver.common.by import By
@@ -20,21 +21,20 @@ class ConfluenceCrawl(CommonCrawl):
     def before_crawl(self, args, browser: WebDriver) -> WebDriver:
         self.next_urls = []
         self.urls = [
-            "https://confluence.shopee.io/pages/viewpage.action?pageId=98003730&src=contextnavpagetreemode"]
+            "https://confluence.shopee.io/pages/viewpage.action?pageId=902020112"]
         for url in self.urls:
             self.result_map[url] = []
         self.file_location = 'confluence_crawl'
         self.is_save_img = True
         self.mode = -1
         self.build_tree(browser)
-
         return browser
 
     def build_tree(self, browser: WebDriver):
+        browser.execute_script(f"location.href='{self.urls[0]}';")
         print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), 'ConfluenceCrawl start crawl..',
               browser.current_url)
         noted = False
-        browser.execute_script(f"location.href='{self.urls[0]}';")
         while "https://confluence.shopee.io/pages/" not in browser.current_url:
             if not noted:
                 print("请登录后操作..")
@@ -43,23 +43,31 @@ class ConfluenceCrawl(CommonCrawl):
         time.sleep(5)
         print("登录成功..")
         # 挑选出/pages/的前缀所有页面
-        root = ConfluenceTree("home", browser.current_url, None)
+        root = load_tree()
+        if root is None:
+            root = ConfluenceTree("root", browser.current_url, 1, -1)
+        print('root init:', root)
         self.recursive_search(root, browser, self.url_map)
         print(root)
         self.tree = root
 
     def parse(self, browser: WebDriver):
-        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), 'ConfluenceCrawl start crawl..',
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), 'ConfluenceCrawl start crawl parse..',
               browser.current_url)
         return
 
     def recursive_search(self, root, browser: WebDriver, url_map):
-        # 点击当前页
-        browser.execute_script(f"location.href='{root.url}';")
-        time.sleep(10)
-        self.save_img(browser, 0, root.name)
         try:
-            if CommonInstance.Redis_client.get(root.url) is None:
+            opt_url = root.url.split("&", 2)[0]
+            if CommonInstance.Redis_client.get(opt_url) is None:
+                while "https://confluence.shopee.io/pages/" not in browser.current_url:
+                    print("请登录后操作..")
+                    time.sleep(1)
+                # 点击当前页
+                browser.execute_script(f"location.href='{root.url}';")
+                time.sleep(2)
+                self.save_img(browser, 0, root.name)
+                time.sleep(8)
                 eles = browser.find_elements(By.XPATH, "//a[contains(@id,'action-menu-link')]")
                 if len(eles) != 0:
                     time.sleep(1)
@@ -70,7 +78,7 @@ class ConfluenceCrawl(CommonCrawl):
                     time.sleep(1)
                     eles[0].click()
                     time.sleep(5)
-                CommonInstance.Redis_client.set(root.url, '')
+                CommonInstance.Redis_client.set(opt_url, '')
 
         except Exception as e:
             print(e, root.url, root.name)
@@ -91,17 +99,57 @@ class ConfluenceCrawl(CommonCrawl):
                     continue
                 url_map[url] = ''
                 title += ' -> ' + url.split('pageId=', 2)[1].split('&', 2)[0]
-                print(url, title)
-                if '周报' in title or '日报' in title:
+                print(url + " " + title)
+                with open("./confluence_crawl/1_file_name.txt", "a") as my_file:
+                    my_file.write(url + " " + title + "\n")
+                if '周报' in title or '日报' in title or '大促计划' in title:
                     continue
-                node = ConfluenceTree(title, url, root)
-                root.children.append(node)
+                node = ConfluenceTree(title, url, root.node_id + 1, root.node_id)
+                root.add_child(node)
                 self.recursive_search(node, browser, url_map)
 
 
 class ConfluenceTree:
-    def __init__(self, name, url, parent):
+    def __init__(self, name, url, node_id, parent_id, is_new=True):
+        self.node_id = node_id
+        self.parent_id = parent_id
         self.name = name
         self.url = url
-        self.parent = parent
+        self.parent = None
         self.children = []
+
+        if is_new:
+            inner_json = json.dumps(self.__dict__)
+            with open("./confluence_crawl/tree.txt", "a") as my_file:
+                my_file.write(inner_json + "\n")
+
+    def add_child(self, node):
+        node.parent = self
+        self.children.append(node)
+
+
+def handle(d):
+    tree = ConfluenceTree(d['name'], d['url'], d['node_id'], d['parent_id'], False)
+    tree.parent_id = d['parent_id']
+    return tree
+
+
+def load_tree():
+    node_map = {}
+    root = None
+    with open("./confluence_crawl/tree.txt", "r+") as my_file:
+        lines = my_file.readlines()
+        for line in lines:
+            if len(line) == 0:
+                return root
+            line = line.replace("\n", "")
+            c = json.loads(line, object_hook=handle)  # type:ConfluenceTree
+            node_map[c.node_id] = c
+        for line in lines:
+            line = line.replace("\n", "")
+            c = json.loads(line, object_hook=handle)  # type:ConfluenceTree
+            if c.parent_id in node_map:
+                node_map[c.parent_id].add_child(c)
+            else:
+                root = c
+    return root
