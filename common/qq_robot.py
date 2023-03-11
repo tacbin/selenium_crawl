a1d16fc122f7642877ee6c0cdb55b1f4b8b6a530
tacbin <2571761894@qq.com>
2023-03-11 16:50:06 +0800

    1111

--- common/qq_robot.py
+++ common/qq_robot.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import hashlib
 import random
 import time
 from typing import List
@@ -14,8 +15,30 @@ class QQRobot:
                 time.sleep(random.randint(5, 15))
             CommonInstance.App_IS_LOCKED = True
             time.sleep(random.randint(1, 3))
-            CommonInstance.QQ_ROBOT.send_group_msg(group=group,
-                                                   msg=msg)
+            msg = msg[0]
+
+            import requests
+
+            url = "http://119.29.97.135:8888/sendgroupmsg"
+
+            payload = '{"group":%d,"logonqq":3266363480,"msg":"%s"}' % (group, msg.encode('utf-8'))
+            curr_stamp = int(time.time()) + 150
+
+            message = "user1/sendgroupmsg" + hashlib.md5("root".encode()).hexdigest() + str(curr_stamp)
+
+            message = hashlib.md5(message.encode('utf-8')).hexdigest()
+
+            res = "user=user1;timestamp=" + str(curr_stamp) + ";signature=" + message
+
+            headers = {
+                'Content-Type': 'application/json',
+                'Cookie': res
+            }
+
+            response = requests.request("POST", url, headers=headers, data=payload)
+
+            print(response.text)
+
         except Exception as e:
             print('send_group_msg', e)
         finally:
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import hashlib
+import time
+
 import crawl_mapping
 from common_crawl import CommonCrawl
 import miraicle
@@ -13,6 +16,42 @@ from middleware.init_middleware import init_middleware
 from threads.qq_robot_thread import QQRobotThreadControl
 
 if __name__ == '__main__':
-    init_middleware()
-    crawl = ConfluenceCrawl()
-    crawl.run()
+    group = 858288116
+    msg = "我复活了"
+
+    import requests
+
+    url = "http://119.29.97.135:8888/sendgroupmsg"
+
+    payload = '{"group":%d,"logonqq":3266363480,"msg":"%s"}' % (group,msg.encode('utf-8'))
+    curr_stamp = int(time.time()) + 150
+
+    message = "user1/sendgroupmsg" + hashlib.md5("root".encode()).hexdigest() + str(curr_stamp)
+
+    message = hashlib.md5(message.encode('utf-8')).hexdigest()
+
+    res = "user=user1;timestamp=" + str(curr_stamp) + ";signature=" + message
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Cookie': res
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+    # import requests
+    #
+    # url = "http://119.29.97.135:8888/sendgroupmsg"
+    #
+    # payload = "{\"group\":858288116,\r\n\"logonqq\":3266363480,\r\n\"msg\":\"ffff\"}"
+    # headers = {
+    #     'Content-Type': 'application/json',
+    #     'Cookie': 'user=user1;timestamp=1678524193;signature=4d7ba933f6a708306ab9b460277b38e0'
+    # }
+    #
+    # response = requests.request("POST", url, headers=headers, data=payload)
+    #
+    # print(response.text)
+
