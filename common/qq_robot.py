0d6ef81f7a658559d6b872cfd31850bca32809fd
tacbin <2571761894@qq.com>
2023-03-22 22:26:55 +0800

    new crawlers

--- common/qq_robot.py
+++ common/qq_robot.py
@@ -20,25 +20,39 @@ class QQRobot:
             print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), 'send..',
                   group, msg)
 
-            url = "http://119.29.97.135:8888/sendgroupmsg"
+            import requests
 
-            payload = '{"group":%d,"logonqq":3266363480,"msg":"%s"}' % (group, msg)
-            curr_stamp = int(time.time()) + 150
+            url = "http://119.29.97.135:8888/send/group?route=105&frameqq=3266363480&group=%s&key=key&newscontent=%s" % (
+                group, msg)
 
-            message = "user1/sendgroupmsg" + hashlib.md5("root".encode()).hexdigest() + str(curr_stamp)
-
-            message = hashlib.md5(message.encode('utf-8')).hexdigest()
-
-            res = "user=user1;timestamp=" + str(curr_stamp) + ";signature=" + message
-
+            payload = {}
             headers = {
-                'Content-Type': 'application/json',
-                'Cookie': res
+                'Content-Type': 'multipart/form-data'
             }
 
-            response = requests.request("POST", url, headers=headers,timeout=30,
-                                        data=payload.replace('&nbsp;', '').replace('\xa0', ' ').encode(
-                                            encoding='utf-8'))
+            response = requests.request("POST", url, headers=headers, data=payload)
+
+            print(response.text)
+
+            # url = "http://119.29.97.135:8888/sendgroupmsg"
+            #
+            # payload = '{"group":%d,"logonqq":3266363480,"msg":"%s"}' % (group, msg)
+            # curr_stamp = int(time.time()) + 150
+            #
+            # message = "user1/sendgroupmsg" + hashlib.md5("root".encode()).hexdigest() + str(curr_stamp)
+            #
+            # message = hashlib.md5(message.encode('utf-8')).hexdigest()
+            #
+            # res = "user=user1;timestamp=" + str(curr_stamp) + ";signature=" + message
+            #
+            # headers = {
+            #     'Content-Type': 'application/json',
+            #     'Cookie': res
+            # }
+            #
+            # response = requests.request("POST", url, headers=headers,timeout=30,
+            #                             data=payload.replace('&nbsp;', '').replace('\xa0', ' ').encode(
+            #                                 encoding='utf-8'))
 
             print(response.text)
 
